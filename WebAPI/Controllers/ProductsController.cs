6c2dd9501afe7bef84f23ddb6c46a4c766bb4056
Cubetron Software <Cubetron Software@LAPTOP-7NOG9R23>
2022-08-09 13:51:37 +0300

    9 Ağustos

---
+++ WebAPI/Controllers/ProductsController.cs
@@ -0,0 +1,59 @@
+﻿using Business.Abstract;
+using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
+using Entities.Concrete;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController] //Attribute 
+    public class ProductsController : ControllerBase
+    {
+        IProductService _productService; //Field oluşturuyoruz
+
+        public ProductsController(IProductService productService)
+        {
+            _productService = productService;
+        }
+
+        [HttpGet("getall")]
+        public IActionResult GetAll() //uydurmasyon bir metot 
+        {
+            //Swagger
+            //Dependency Chain = Bağımlılık zinciri
+            var result = _productService.GetAll();
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
+
+        [HttpGet("getbyid")]
+        public IActionResult GetById(int id)
+        {
+            var result = _productService.GetById(id);
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
+
+        [HttpPost("add")]
+        public IActionResult Add(Product product)
+        {
+            var result = _productService.Add(product);
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
+
+
+    }
+}
