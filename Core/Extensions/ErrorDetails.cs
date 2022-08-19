d4f701fda74dfe16ea06ea23cf22e19ba0858357
Cubetron Software <Cubetron Software@LAPTOP-7NOG9R23>
2022-08-19 17:05:46 +0300

    Frontend 5.Bölüm Sonu

--- Core/Extensions/ErrorDetails.cs
+++ Core/Extensions/ErrorDetails.cs
@@ -1,6 +1,8 @@
-﻿using Newtonsoft.Json;
+﻿using FluentValidation.Results;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
+using System.Reflection.Metadata.Ecma335;
 using System.Text;
 
 namespace Core.Extensions
@@ -9,10 +11,16 @@ namespace Core.Extensions
     {
         public string Message { get; set; }
         public int StatusCode { get; set; }
+        public IEnumerable<ValidationFailure> Errors { get; set; }
 
         public override string ToString()
         {
             return JsonConvert.SerializeObject(this);
         }
+    }
+
+    public class ValidationErrorDetails : ErrorDetails
+    {
+        public IEnumerable<ValidationFailure> Errors { get; set; }
     }
 }
